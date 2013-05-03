41f4b33492a767bb1da89a00e0944a24fe7b2979
Koushik Dutta <koushd@gmail.com>
2013-05-02 22:58:30 -0700

    Reverse kang Wil Wheaton Recovery enhancements.
    Change-Id: Ia9eace0ff0826801515b335ac6543c1271948612

--- extendedcommands.c
+++ extendedcommands.c
@@ -1560,7 +1560,9 @@ int verify_root_and_recovery() {
         }
     }
 
+    int exists = 0;
     if (0 == lstat("/system/bin/su", &st)) {
+        exists = 1;
         if (S_ISREG(st.st_mode)) {
             if ((st.st_mode & (S_ISUID | S_ISGID)) != (S_ISUID | S_ISGID)) {
                 ui_show_text(1);
@@ -1573,6 +1575,7 @@ int verify_root_and_recovery() {
     }
 
     if (0 == lstat("/system/xbin/su", &st)) {
+        exists = 1;
         if (S_ISREG(st.st_mode)) {
             if ((st.st_mode & (S_ISUID | S_ISGID)) != (S_ISUID | S_ISGID)) {
                 ui_show_text(1);
@@ -1581,6 +1584,16 @@ int verify_root_and_recovery() {
                     __system("chmod 6755 /system/xbin/su");
                 }
             }
+        }
+    }
+
+    if (!exists) {
+        ui_show_text(1);
+        ret = 1;
+        if (confirm_selection("Root access is missing. Root device?", "Yes - Root device (/system/xbin/su)")) {
+            __system("cp /sbin/su /system/xbin/su");
+            __system("chmod 6755 /system/xbin/su");
+            __system("ln -sf /system/xbin/su /system/bin/su");
         }
     }
 
