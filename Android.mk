44ccbaba020bf866f1a8698ae09228c55e574aa8
Tom Marshall <tdm@cyngn.com>
2014-10-01 14:49:05 -0700

    sr: Use pathmap for recovery
    Change-Id: I6350618e1e04819d52f1049f9f2fb61c833a1b45

--- Android.mk
+++ Android.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(call my-dir),$(call project-path-for,recovery))
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -155,3 +157,5 @@ include $(LOCAL_PATH)/minui/Android.mk \
     $(LOCAL_PATH)/updater/Android.mk \
     $(LOCAL_PATH)/update_verifier/Android.mk \
     $(LOCAL_PATH)/applypatch/Android.mk
+
+endif
